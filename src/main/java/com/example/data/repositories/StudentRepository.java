29d12b90ac9faf8f35048ff1e881dd3290818263
Lazaruss <Lazaruss@HAL-9015>
2017-10-02 23:34:15 +0200

    Fixed repository for better explanation

--- src/main/java/com/example/data/repositories/StudentRepository.java
+++ src/main/java/com/example/data/repositories/StudentRepository.java
@@ -1,8 +1,16 @@
 package com.example.data.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import com.example.data.entities.Student;
 
 public interface StudentRepository extends CrudRepository<Student, Long>{
+	
+	public Student findByEmail(final String email);
+	
+	@Query("select s from Student s where s.email = :param")
+	public Student mySelect2(@Param("param") final String email);
 
 }
